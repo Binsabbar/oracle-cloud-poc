4ce5a1e0ece6068e27b881a755ce0739e76bee4d
binsabbar <ibinsabbar@gmail.com>
2020-07-07 23:48:21 +0300

    add common configs

---
+++ instance-options.tf
@@ -0,0 +1,10 @@
+locals {
+  images_ids = {
+    ubuntu_20 = "ocid1.image.oc1.me-jeddah-1.aaaaaaaay5jjjjj5bv2hh5553oi2ljo7nc36dxhx75sarcecs5ozlu374lja"
+  }
+
+  shapes = {
+    small    = "VM.Standard.E2.1.Micro"
+    standard = "VM.Standard.E2.1"
+  }
+}
