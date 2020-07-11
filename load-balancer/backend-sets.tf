d4742ac174c35633179757659a4c41b3d6f89e84
binsabbar <ibinsabbar@gmail.com>
2020-07-11 22:38:37 +0300

    WIP: merge files

--- load-balancer/backend-sets.tf
+++ load-balancer/backend-sets.tf
@@ -18,3 +18,10 @@ resource "oci_load_balancer_backend_set" "backend_set" {
   #     verify_peer_certificate = true
   # }
 }
+
+resource "oci_load_balancer_backend" "backend" {
+  load_balancer_id = ci_load_balancer_load_balancer.load_balancer.id
+  backendset_name  = oci_load_balancer_backend_set.backend_set.name
+  ip_address       = var.backend.ip
+  port             = var.backend.port
+}
