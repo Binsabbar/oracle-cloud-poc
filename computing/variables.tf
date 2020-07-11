f672b79d8745d29813380cd95204b2056334c05c
binsabbar <ibinsabbar@gmail.com>
2020-07-11 22:27:16 +0300

    fix formatting

--- computing/variables.tf
+++ computing/variables.tf
@@ -2,7 +2,7 @@ variable "compartment" { type = any }
 variable "project_name" { type = string }
 variable "environment" { type = string }
 variable "vnc" { type = any }
-variable "availability_domain" { type = map(any)}
+variable "availability_domain" { type = map(any) }
 variable "instances" {
   type        = map
   description = "map of instances configurations"
