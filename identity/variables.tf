ecbe25d420fd921ccf468ec2c36478142fca59bd
binsabbar <ibinsabbar@gmail.com>
2020-07-12 22:34:31 +0300

    define type for variables - identity

--- identity/variables.tf
+++ identity/variables.tf
@@ -1,10 +1,7 @@
 variable "project_name" { type = string }
 variable "environment" { type = string }
 variable "compartments" {
-  type = map
-  default = {
-    "main" = {
-      root = "rootCompartment"
-    }
-  }
+  type = map(object({
+    root = string
+  }))
 }
