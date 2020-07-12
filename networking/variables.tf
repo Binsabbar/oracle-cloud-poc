2eed7119bdfbc5487c2eb22d2d9a584bb69bee91
binsabbar <ibinsabbar@gmail.com>
2020-07-12 22:35:12 +0300

    define type for variables - networking

--- networking/variables.tf
+++ networking/variables.tf
@@ -3,14 +3,14 @@ variable "project_name" { type = string }
 variable "environment" { type = string }
 variable "dns_label" { type = string }
 variable "allowed_ingress_ports" {
-  type    = list
+  type    = list(number)
   default = [80, 443]
 }
 variable "public_net_security_list_ids" {
-  type    = list
+  type    = list(string)
   default = []
 }
 variable "private_net_security_list_ids" {
-  type    = list
+  type    = list(string)
   default = []
 }
