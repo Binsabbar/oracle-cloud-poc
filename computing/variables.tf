ac651ef46974807f1afa5ea26a36344ea4d77f5d
binsabbar <ibinsabbar@gmail.com>
2020-07-12 22:31:57 +0300

    define type for variables - computing

--- computing/variables.tf
+++ computing/variables.tf
@@ -1,10 +1,37 @@
-variable "compartment" { type = any }
 variable "project_name" { type = string }
 variable "environment" { type = string }
-variable "vnc" { type = any }
-variable "availability_domain" { type = map(any) }
+variable "availability_domain" {
+  type = object({
+    name = string
+  })
+}
+
+variable "compartment" {
+  type = object({
+    id = string
+  })
+}
+
+variable "vnc" {
+  type = object({
+    id = string
+  })
+}
+
 variable "instances" {
-  type        = map
+  type = map(object({
+    volume_size     = number
+    autherized_keys = string
+    config = object({
+      shape           = string
+      image_id        = string
+      network_sgs_ids = list(string)
+      subnet = object({
+        id                         = string,
+        prohibit_public_ip_on_vnic = bool
+      })
+    })
+  }))
   description = "map of instances configurations"
 }
 
