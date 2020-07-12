51c2dfaa88b7bbaccd97bf39c9f1c8edd3ab1a01
binsabbar <ibinsabbar@gmail.com>
2020-07-12 22:37:31 +0300

    define type for variables - security-groups

--- security-groups/variables.tf
+++ security-groups/variables.tf
@@ -1,9 +1,20 @@
-variable "safe_ips" {
-  type    = list
-  default = [""]
-}
-
-variable "vnc" { type = any }
-variable "compartment" { type = any }
 variable "project_name" { type = string }
 variable "environment" { type = string }
+
+variable "vnc" {
+  type = object({
+    id         = string
+    cidr_block = string
+  })
+}
+
+variable "compartment" {
+  type = object({
+    id = string
+  })
+}
+
+variable "safe_ips" {
+  type    = list(string)
+  default = [""]
+}
